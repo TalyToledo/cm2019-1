34dee2329930f467828a71e5e54e7d0543055cd4
Aketzally Toledo R <aketzally.toledor@gmail.com>
2018-09-26 12:35:22 -0500

    Add files via upload
    Segue manuales

--- Lessons/Navigation/Navigation/ViewController.swift
+++ Lessons/Navigation/Navigation/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segueSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func segueButton(_ sender: Any) {
+        if segueSwitch.isOn{
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
+        else{
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -24,6 +35,7 @@ class ViewController: UIViewController {
         
     }
 
+    
 
 }
 
