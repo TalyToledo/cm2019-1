6e82a6ec8800353104d1317bdcbcee8502736419
Aketzally Toledo R <aketzally.toledor@gmail.com>
2018-10-15 12:59:21 -0500

    Add files via upload

---
+++ Lessons/PersonalityQuiz/Protocols.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Shoe: CustomStringConvertible{
+    var description: String {
+        return "Shoe(color: \(color), size: \(size), hasLaces: \(hasLaces))"
+    }
+    
+    let color: String
+    let size: Int
+    let hasLaces: Bool
+    
+    init (color: String, size: Int, hasLaces: Bool){
+        self.color = color
+        self.size = size
+        self.hasLaces = hasLaces
+    }
+}
+
+let myShoe = Shoe(color: "Black", size: 12, hasLaces: true)
+print(myShoe)
+  
