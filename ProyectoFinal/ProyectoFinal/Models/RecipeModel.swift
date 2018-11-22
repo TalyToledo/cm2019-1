2d297bac2c9bb28fd065f3d56bbaf16ed90fbd30
Aketzally Toledo R <aketzally.toledor@gmail.com>
2018-11-22 12:46:01 -0600

    Add files via upload

--- ProyectoFinal/ProyectoFinal/Models/RecipeModel.swift
+++ ProyectoFinal/ProyectoFinal/Models/RecipeModel.swift
@@ -8,17 +8,35 @@
 
 import Foundation
 
-struct Receta {
+struct Receta: Codable {
     var titulo: String
-    var ingredientes: [Ingrediente]
+    //var ingredientes: [Ingrediente]
     var pasos: [String]
     var tiempoEnMinutos: Int
     var tipoCocina: String
-    var dificultad: String
+    //var dificultad: String
+    
+    enum CodingKeys: String, CodingKey {
+        case titulo = "title"
+        //case ingredientes = "extendedIngridientes"
+        case pasos = "steps"
+        case tiempoEnMinutos = "readyInMinutes"
+        case tipoCocina = "cuisines"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        titulo = try values.decode(String.self, forKey: CodingKeys.titulo)
+        //ingredientes = try values.decode(String.self, forKey: CodingKeys.ingredientes)
+        pasos = try values.decode([String].self, forKey: CodingKeys.pasos)
+        tiempoEnMinutos = try values.decode(Int.self, forKey: CodingKeys.tiempoEnMinutos)
+        tipoCocina = try values.decode(String.self, forKey: CodingKeys.tipoCocina)
+        
+    }
     
 }
 
-struct Ingrediente {
+struct Ingrediente: Codable {
     var nombre: String
     var cantidad: Float
     var unidades: String
